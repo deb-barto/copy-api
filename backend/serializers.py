8ee6860f7814afb02429254d8e2b479389876049
hana-bananaa <debartosiaki@gmail.com>
2024-03-25 02:08:30 -0300

    little fix line

--- backend/serializers.py
+++ backend/serializers.py
@@ -11,7 +11,6 @@ from rest_framework import serializers
 class DynamicFieldsSerializer(serializers.Serializer):
     def __init__(self, *args, **kwargs):
         data = kwargs.get('data')
-        
         if isinstance(data, dict):
             for field_name, field_value in data.items():
                 field_class = serializers.CharField if isinstance(field_value, str) else serializers.IntegerField
