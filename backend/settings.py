3c0d8475ba9f5748924efa5cd472f5abbedcd53f
hana-bananaa <debartosiaki@gmail.com>
2024-03-25 12:29:27 -0300

    ajustes setings

--- backend/settings.py
+++ backend/settings.py
@@ -34,9 +34,9 @@ CORS_ORIGIN_WHITELIST = (
     'https://copy-app-sigma.vercel.app/'
 )
 
-CORS_ALLOWED_ORIGINS = [
-    'https://copy-app-sigma.vercel.app/'
-]
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = True
 
 CORS_ALLOW_METHODS = [
     "DELETE",
@@ -81,9 +81,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
